0a84f4606250257c5857b44adb931bc21eb754c8
jon-xavier <jonathan.xavier0@gmail.com>
2015-06-05 06:20:33 +0000

    updated with templates

--- hello_word.py
+++ hello_word.py
@@ -1,26 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
+from jinja2 import Template
 
 app = Flask(__name__)
 
-@app.route("/hello")
-def say_hi():
-    return "Hello World!"
+@app.route("/")
+def template_test():
+    return render_template('template.html', my_string="Wheeeee!",
+                           my_list=[0,1,2,3,4,5])
 
 @app.route("/hello/<name>")
 def hello_person(name):
-    html = """
-        <h1>
-            Hello {}!
-        </h1>
-        <p>
-            Here's a picture of a kitten.  Awww...
-        </p>
-        <img src="http://placekitten.com/g/200/300">
-    """
-    return html.format(name.title())
+    return render_template('hello_person.html', name = name)
 
 @app.route("/jedi/<first_name>/<last_name>")
-def jedi_name(first_name, last_name):
+def jedi_name(first_name, last_name): 
     jedi_name = last_name[0:2] + first_name[0:2]
     html = """
     <p>
